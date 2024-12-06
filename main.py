f735ae3dc1706a7921a28d8f674c987d5cba5be3
Sara Francis <saradf.paz@gmail.com>
2024-12-06 15:59:30 -0500

    Updated main to run FA stuff

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import gymnasium as gym
 import ale_py
+import time
 
-from pacmanEnv import PacMan
+# from pacmanEnv import PacMan
 from pacman_dql import pacmanDQL
+from pacman_SARSA import FunctionApproximation, ObservationWrapper
 
-def createEnv():
+def createDQLEnv():
     # env = PacMan()
     env = gym.make('ALE/MsPacman-v5', render_mode=None, obs_type="grayscale")
-    executePacmanDQL(env)
+    pacmanDQL(env)
     env.close()
 
-def executePacmanDQL(env):
-    # Hyperparameters
+def runDQL():
+    createDQLEnv()
 
-    pacmanDQL(env)
+def runFunctionApproximation():
+    pacMan = FunctionApproximation()
+    
+    env = gym.make('ALE/MsPacman-v5', obs_type="ram", render_mode=None)
+    env = ObservationWrapper(env)
+
+    start = time.time()
+
+    # SARSA
+    w = pacMan.SARSA(env, 50000)
+
+    # Q-Learning
+    # w = pacMan.QLearning(env, 50000)
+    
+    end = time.time()
+    print(f"Took {end-start:.3f} seconds")
+    env.close()
+
+    # uncomment if testing
+    # input("Press the Enter key to continue: ") 
+    # pacMan.test(w)
+
 
 if __name__ == "__main__":
-    createEnv()
+    ### DQL (Dominic) ###
+    # runDQL()
+    
+    ### Function Approximation (Sara) ###
+    runFunctionApproximation()
